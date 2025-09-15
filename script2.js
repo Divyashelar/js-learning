7d29d8a2abc754b942f12d832158defe2dacba70
Divya shelar <divyashelar90@gmail.com>
2025-09-15 14:48:01 +0530

    updatind

---
+++ script2.js
@@ -0,0 +1,20 @@
+//Artimetic Operator
+let a = 5;
+let b = 2;
+
+//console.log("a+b =", a+b);
+//console.log("a-b =", a-b);
+console.log("a =",a, "& b =",b);
+//console.log("a*b =", a*b);
+//console.log("a/b =", a/b);
+//console.log("a%b =", a%b);// modulus
+//console.log("a**b =", a**b);//Exponentiation
+
+//unary Operator
+//a=a+1;
+//a++;     post increment
+//console.log("++a=",++a); //pre increment
+//console.log(a)
+
+//console.log("a--=",a--); //post decrement
+//console.log(a)
